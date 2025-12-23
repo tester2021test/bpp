31761bbaf648004d02e033bd048e45bbbfc3c527
tester2021test <69193224+tester2021test@users.noreply.github.com>
2025-12-23 18:47:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// REPLACE 'repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
+// Example: If your repo is [https://github.com/username/my-sec-app](https://github.com/username/my-sec-app), use '/my-sec-app/'
 export default defineConfig({
   plugins: [react()],
+  base: '/bpp/', 
 })
+
